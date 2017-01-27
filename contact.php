ea756b51f1118007e0adb5882d5566dd142e1989
Angel Angeles <angelangelesiii@gmail.com>
2017-01-28 07:18:08 +0800

    Updated contact-us template

--- contact.php
+++ contact.php
@@ -22,20 +22,46 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<div class="wrapper">
-			<h1>TEMPLATE CONTACT US</h1>
-			<?php
-				while ( have_posts() ) : the_post();
-				
-					get_template_part( 'template-parts/content', 'page' );
-				
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
-				
-				endwhile; // End of the loop.
-				?>
-				
+			<?php while(have_posts()) { 
+				the_post();
+			?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content contact-us-content row">
+						<div class="page-content large-7 medium-6 small-12 column">
+						<?php
+							the_content();
+						?>
+						</div>
+						<div class="page-meta large-5 medium-6 small-12 column">
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit voluptatum sapiente quam debitis ut modi alias quae perferendis ratione, nihil fugiat, ab veniam neque. Incidunt.</p>
+						</div>
+					</div><!-- .entry-content -->
+					
+
+
+
+					<?php if ( get_edit_post_link() ) : ?>
+						<footer class="entry-footer">
+							<?php
+								edit_post_link(
+									sprintf(
+										/* translators: %s: Name of current post */
+										esc_html__( 'Edit %s', 'asiapacificairways' ),
+										the_title( '<span class="screen-reader-text">"', '"</span>', false )
+									),
+									'<span class="edit-link">',
+									'</span>'
+								);
+							?>
+						</footer><!-- .entry-footer -->
+					<?php endif; ?>
+				</article><!-- #post-## -->
+			
+			<?php } ?>
 			</div>
 
 		</main><!-- #main -->
